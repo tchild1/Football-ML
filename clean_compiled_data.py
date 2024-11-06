cc0be5a249cfc9165fa581c7c59defbe7dd50dc8
Sam Vance <samvance243@gmail.com>
2024-11-06 11:11:39 -0700

    dropped extra rows

---
+++ clean_compiled_data.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df = pd.read_csv('./compiledData/compliedData.csv')
+# count number of nan values in each column and print those with more than 0
+print(df.columns)
+for column in df.columns:
+    if df[column].isnull().sum() > 1000:
+        print(column, " has ", df[column].isnull().sum(), " nan values... Dropping column")
+        # drop columns with more than 1000 nan values
+        df.drop(column, axis=1, inplace=True)
+print("Total rows: ", len(df))
+
+# save the cleaned data
+df.to_csv('./compiledData/cleanedCompiledData.csv', index=False)
