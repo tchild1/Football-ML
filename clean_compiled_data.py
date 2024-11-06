e4c44dc0b8629e80a46316d13f0ed1a1ec113e49
Sam Vance <samvance243@gmail.com>
2024-11-06 11:16:02 -0700

    added analysis of unique values

--- clean_compiled_data.py
+++ clean_compiled_data.py
@@ -10,5 +10,10 @@ for column in df.columns:
         df.drop(column, axis=1, inplace=True)
 print("Total rows: ", len(df))
 
+
+# check the quantity of unique values in each column
+for column in df.columns:
+    print(column, " has ", df[column].nunique(), " unique values")
+
 # save the cleaned data
 df.to_csv('./compiledData/cleanedCompiledData.csv', index=False)
